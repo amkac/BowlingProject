0b3a0919b63050ac8d8f5a785876f7c63d4527b9
rottanaly <rottanaly@ymail.com>
2016-03-09 11:35:22 +0100

    Bowling commit

--- src/main/java/dcll/akac/Frame.java
+++ src/main/java/dcll/akac/Frame.java
@@ -4,27 +4,30 @@ package dcll.akac;
  * Created by rottanaly on 3/8/16.
  */
 public class Frame {
-    public int firstTry = 0;
-    public int secondTry = 0;
+    private int firstTry = 0; // score de preimier essai
+    private int secondTry = 0; // score de deuxieme essai
 
-    public Frame(int a, int b){
+    /* Constructeur */
+    public Frame(final int a, final int b) {
         firstTry = a;
         secondTry = b;
     }
 
-    int getFirstScore(){
+    /* Acceder au score de premier essai */
+    int getFirstScore() {
         return firstTry;
     }
 
+    /* Score totale de chaque frame */
     int getTotalScore(){
         return firstTry + secondTry;
     }
 
-    int getStatus(){
-        if(getFirstScore()==10){
+    int getStatus() {
+        if(getFirstScore()==10) {
             return 2; // strike
         }
-        if(getTotalScore()==10){
+        if(getTotalScore()==10) {
             return 1; // spare
         }
         return 0; // normal
--- src/main/java/dcll/akac/ScoreBoard.java
+++ src/main/java/dcll/akac/ScoreBoard.java
@@ -10,19 +10,28 @@ public class ScoreBoard {
         int TotalScore = 0;
 
         int[] tempBoard = new int[12];
-        for(int i = 0; i<12; i++){
+        for(int i = 0; i<12; i++)
+        {
+            // tempBoard: un taleau qui contient le score temporaire
+            // allFrame.get(i) va retourner la Frame i
             tempBoard[i] = allFrame.get(i).getTotalScore();
         }
-        for(int i = 0; i<10; i++){
-            if(allFrame.get(i).getStatus()==2){
-                if(allFrame.get(i+1).getStatus() != 2){
-                    TotalScore += tempBoard[i] + tempBoard[i+1];
-                }else{
-                    TotalScore += tempBoard[i] + tempBoard[i+1] + allFrame.get(i+2).getFirstScore();
+        for(int i = 0; i<10; i++) {
+            // Cas "Strike"
+            if(allFrame.get(i).getStatus()==2) {
+                // Total score = Score courant + les deux prochaines scores
+                TotalScore += tempBoard[i] + tempBoard[i+1];
+                // Si le prochain Frame est Strike, on doit ajouter encore d'autre Score
+                if(allFrame.get(i+1).getStatus() == 2){
+                    TotalScore += allFrame.get(i+2).getFirstScore();
                 }
-            }else if(allFrame.get(i).getStatus()==1){
+            }
+            // Cas "Spare"
+            else if(allFrame.get(i).getStatus()==1) {
                 TotalScore += tempBoard[i] + allFrame.get(i+1).getFirstScore();
-            }else{
+            }
+            // Cas "Normal"
+            else {
                 TotalScore += tempBoard[i];
             }
         }
